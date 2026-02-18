e53cea7247ff0d28dc1fdd2317dc7facfa584393
ErisDai <daigurenricz@outlook.com>
2026-02-17 20:18:31 -0600

    prueba

---
+++ app/src/main/java/udelp/edu/myapplication2/LoginActivity.kt
@@ -0,0 +1,14 @@
+package udelp.edu.myapplication2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
