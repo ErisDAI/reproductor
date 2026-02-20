105ea0607291f01c7bca66337309c52ee2ff734b
ErisDai <daigurenricz@outlook.com>
2026-02-19 21:28:03 -0600

    Tarea 2

--- app/src/main/java/udelp/edu/myapplication2/LoginActivity.kt
+++ app/src/main/java/udelp/edu/myapplication2/LoginActivity.kt
@@ -1,6 +1,9 @@
 package udelp.edu.myapplication2
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,6 +12,22 @@ import androidx.core.view.WindowInsetsCompat
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val aceptarButton: Button = findViewById<Button>(R.id.aceptarLogin)
+        aceptarButton.setOnClickListener {
+           doLogin()
+        }
+    }
+    fun doLogin() {
+        val username : EditText = findViewById<EditText>(R.id.username)
+        val password : EditText = findViewById<EditText>(R.id.password)
+        if (username.getText().isNotEmpty() && password.getText().isNotEmpty()) {
+            if(username.getText().toString()=="admin" && password.getText().toString()=="123"){
 
+            }else{
+                Toast.makeText(this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show()
+            }
+        }else{
+            Toast.makeText(this, "Por favor ingrese un usuario y contraseña", Toast.LENGTH_SHORT).show()
+        }
     }
 }
