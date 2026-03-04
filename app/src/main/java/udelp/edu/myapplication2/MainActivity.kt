3223f1db6b243fc39237fe86849b400a25457cec
ErisDai <daigurenricz@outlook.com>
2026-03-03 21:13:44 -0600

    act4

--- app/src/main/java/udelp/edu/myapplication2/MainActivity.kt
+++ app/src/main/java/udelp/edu/myapplication2/MainActivity.kt
@@ -6,11 +6,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
         loadFragment(HomeFragment())
     }
     private fun loadFragment(fragment: Fragment) {
